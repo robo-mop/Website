8a77f604aa8cefe9a5c5c323271ad5f8efe531b0
robo-mop <realaryanpingle@gmail.com>
2021-05-31 13:11:36 +0530

    Performance upgrade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 function setup()
 {
+    // Add signals for all showcases
+    document.querySelectorAll(".showcase").forEach(showcase=>{
+        let indicator = showcase.nextElementSibling;
+        for(let i = 0; i < showcase.childElementCount; ++i)
+        {
+            let signal = document.createElement("DIV");
+            signal.classList.add("signal");
+            indicator.appendChild(signal);
+        }
+    });
     document.querySelectorAll(".showcase").forEach((showcase)=>{
         showcase.addEventListener("scroll", ()=>{
             let firstChild = showcase.children[0];
@@ -8,7 +18,7 @@ function setup()
             let each = 1 / (showcase.childElementCount-1); // 33% for 4 elements
             let index1 = parseInt(perc / each);
             let op1 = perc % each;
-            let a = document.querySelectorAll(".indicators > .signal");
+            let a = showcase.nextElementSibling.children;
             a[index1].style.opacity = map(op1, 0, each, 1, 0.2);
             a[index1+1].style.opacity = map(op1, 0, each, 0.2, 1);
             for(let i = 0; i < showcase.childElementCount; ++i)
